fedd8606230aea5ae2668afdceb7f88242a9ac82
illustriousgeorge <georgios.hraba@educanet.cz>
2020-12-13 22:41:39 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+
+        User.Builder pepa = new User.Builder("illgee","password123").age(123).name("Ill").surname("Gee").gender(true).phone(123456789);
+    }
+
+}
